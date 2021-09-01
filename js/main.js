1f85720df63644cc4d8200a3d665be940a7b39eb
Clara Vilela <claravilelajato@gmail.com>
2021-09-01 10:19:15 +0200

    Try functions and event

--- js/main.js
+++ js/main.js
@@ -11,3 +11,31 @@ const inputNumber = document.querySelector('.js-input');
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
+//counter
+let counter = 0;
+function writeTextCounter() {
+  textCounter.innerHTML = `número de intentos` + (counter += 1);
+}
+//attempts
+function writeNumber() {
+  const number = parseInt(inputNumber.value);
+  if (number <= 1 || number >= 100) {
+    console.log('El número debe estar entre 1 y 100');
+  } else if (number === randomNumber) {
+    console.log('¡¡¡Has ganado campeona!!!');
+  } else if (number > randomNumber) {
+    console.log('Demasiado alto');
+  } else if (number < randomNumber) {
+    console.log('Demasiado bajo');
+  }
+}
+
+//function to handle listener
+function handleClickButton(event) {
+  event.preventDefault();
+  writeTextCounter();
+  writeNumber();
+}
+
+//event listener
+button.addEventListener('click', handleClickButton);
