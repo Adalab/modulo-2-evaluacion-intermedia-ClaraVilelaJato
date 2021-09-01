8c920b0cd865271441e1f9a3890dd27f59194590
Clara Vilela <claravilelajato@gmail.com>
2021-09-01 09:47:07 +0200

    Add variables js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 'use strict';
+//const
 
 const randomNumber = getRandomNumber(100);
 console.log('El número aleatorio es', randomNumber);
+const button = document.querySelector('.js-btn');
+const textHint = document.querySelector('.js-hint');
+const textCounter = document.querySelector('.js-counter');
+const inputNumber = document.querySelector('.js-input');
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
